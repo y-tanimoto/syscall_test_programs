7b7c80bcd7bc8d808abc5769aeef84c4a129646f
ytani <tanimoto@asg.cs.tuat.ac.jp>
2022-05-10 12:16:02 +0900

    fix test_sandbox.c

--- test_sandbox.c
+++ test_sandbox.c
@@ -11,23 +11,37 @@ int main() {
   }
   else {
     printf("pid=%d\n", pid);
-    
     int a;
+
+    // 子スレッドをサンドボックスに入れる
     a = syscall(459, pid);
     printf("register: %d\n", a);
 
+    // Syscall1と2を禁止
     a = syscall(460, pid, 1, 0);
     printf("set syscall-1 to forbidden: %d\n", a);
     a = syscall(460, pid, 2, 0);
     printf("set syscall-2 to forbidden: %d\n", a);
-    a = syscall(460, pid, 2, 1);
-    printf("set syscall-2 to allow: %d\n", a);
 
+    // サンドボックスに入っているかチェック＆禁止リストをログに表示
     a = syscall(462, pid);
     printf("exists: %d\n", a);
 
+    // Syscall2を許可
+    a = syscall(460, pid, 2, 1);
+    printf("set syscall-2 to allow: %d\n", a);
+
+    // 禁止リストをログに表示
+    a = syscall(462, pid);
+    printf("exists: %d\n", a);
+
+    // サンドボックスから取り出す
     a = syscall(461, pid);
     printf("delete profile: %d\n", a);
+
+    // サンドボックスに入っているかかチェック
+    a = syscall(462, pid);
+    printf("exists: %d\n", a);
 
     wait(NULL);
   }
