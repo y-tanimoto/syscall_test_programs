78f472a999f8d78483f48c297d3dc70eef928e3d
ytani <tanimoto@asg.cs.tuat.ac.jp>
2022-05-10 15:18:24 +0900

    complete test_simple_proc.c

--- test_simple_proc.c
+++ test_simple_proc.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void parent(int pid) {
-  
-}
+struct user_simple_proc_struct {
+  int pid;
+  int ppid;
+  char cmd[20];
+};
 
 int main() {
   int pid, pid2, ppid, a;
   char *cmd, *cmd2;
+  struct user_simple_proc_struct *usp;
 
   ppid = getpid();
 
@@ -20,8 +23,10 @@ int main() {
   cmd = "emacs Hello";
   a = syscall(463, pid, ppid, cmd);               // set_simple_proc
   printf("ppid=%d, pid=%d: set_simple_proc returned %d.\n", ppid, pid, a);
-  a = syscall(465, pid);                          // search_simple_proc
-  printf("ppid=%d, pid=%d: search_simple_proc returned %d.\n", ppid, pid, a); 
+  a =  (struct simple_proc_struct*)syscall(465, pid, usp);                          // search_simple_proc
+  printf("ppid=%d, pid=%d: search_simple_proc returned %d.\n", ppid, pid, a);
+  if (usp != NULL)
+    printf("child proc 1 profile:\npid=%d\nppid=%d\ncmd=%s\n", usp->pid, usp->ppid, usp->cmd);
 
   pid2 = fork();
   if (pid2 == 0) {
@@ -31,18 +36,20 @@ int main() {
 
   cmd2 = "ls";
   a = syscall(463, pid2, ppid, cmd2);             // set_simple_proc
-  printf("ppid=%d, pid=%d: set_simple_proc returned %d.\n", ppid, pid, a);
-  a = syscall(465, pid2);                         // search_simple_proc
-  printf("ppid=%d, pid=%d: search_simple_proc returned %d.\n", ppid, pid, a); 
-
+  printf("ppid=%d, pid=%d: set_simple_proc returned %d.\n", ppid, pid2, a);
+  a =  (struct simple_proc_struct*)syscall(465, pid2, usp);                         // search_simple_proc
+  printf("ppid=%d, pid=%d: search_simple_proc returned %d.\n", ppid, pid2, a);
+  if (usp != NULL)
+    printf("child proc 1 profile:\npid=%d\nppid=%d\ncmd=%s\n", usp->pid, usp->ppid, usp->cmd);
+  
   a = syscall(464, pid);                          // delete_simple_proc
   printf("ppid=%d, pid=%d: delete_simple_proc returned %d.\n", ppid, pid, a); 
-  a = syscall(465, pid);                          // search_simple_proc
+  a = (struct simple_proc_struct*)syscall(465, pid, usp);                          // search_simple_proc
   printf("ppid=%d, pid=%d: search_simple_proc returned %d.\n", ppid, pid, a);
   
   a = syscall(464, pid2);                         // delete_simple_proc
   printf("ppid=%d, pid=%d: delete_simple_proc returned %d.\n", ppid, pid2, a);
-  a = syscall(465, pid2);                         // search_simple_proc
+  a =  (struct simple_proc_struct*)syscall(465, pid2, usp);                         // search_simple_proc
   printf("ppid=%d, pid=%d: search_simple_proc returned %d.\n", ppid, pid2, a); 
 
   return 0;
