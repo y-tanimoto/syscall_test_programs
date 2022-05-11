841822e42c47a361797227b9d1d8ee8517f92656
ytani <tanimoto@asg.cs.tuat.ac.jp>
2022-05-12 00:24:17 +0900

    test_sandbox: run 3 syscalls in child

--- test_sandbox.c
+++ test_sandbox.c
@@ -9,9 +9,15 @@ int main() {
     sleep(3);
 
     // syscall-451を実行
-    int b = -1;
+    int a = 10, b;
     b = syscall(451);
     printf("syscall-451 returned %d\n", b);
+
+    b = syscall(452);
+    printf("syscall-452 returned %d\n", b);
+
+    b = syscall(453, &a);
+    printf("syscall-452 returned %d\n", b);
     
     return 0;
   }
@@ -25,23 +31,25 @@ int main() {
 
     // Syscall1と2を禁止
     a = syscall(460, pid, 451, 0);
-    printf("set syscall-1 to forbidden: %d\n", a);
+    printf("set syscall-451 to forbidden: %d\n", a);
     a = syscall(460, pid, 452, 0);
-    printf("set syscall-2 to forbidden: %d\n", a);
+    printf("set syscall-452 to forbidden: %d\n", a);
 
     // サンドボックスに入っているかチェック＆禁止リストをログに表示
     a = syscall(462, pid);
     printf("exists: %d\n", a);
 
     // Syscall2を許可
-    /*
-    a = syscall(460, pid, 2, 1);
-    printf("set syscall-2 to allow: %d\n", a);
+    a = syscall(460, pid, 452, 1);
+    printf("set syscall-452 to allow: %d\n", a);
+
+    a = syscall(460, pid, 453, 1);
+    printf("set syscall-453 to allow: %d\n", a);
 
     // 禁止リストをログに表示
     a = syscall(462, pid);
     printf("exists: %d\n", a);
-
+    /*
     // サンドボックスから取り出す
     a = syscall(461, pid);
     printf("delete profile: %d\n", a);
