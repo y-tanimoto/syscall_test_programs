93a67169222709ad1513a1e710524319c8c12748
ytani <tanimoto@asg.cs.tuat.ac.jp>
2022-05-15 18:44:50 +0900

    update test_sandbox.c

--- test_sandbox.c
+++ test_sandbox.c
@@ -43,15 +43,15 @@ int child(int sem_id) {
   // syscall451: 実効不可
   int a = 10, b;
   b = syscall(451);
-  printf("syscall-451 returned %d\n", b);
+  printf("pid %d: syscall-451 returned %d\n", getpid(), b);
 
   // syscall452: 実行可能
   b = syscall(452);
-  printf("syscall-452 returned %d\n", b);
+  printf("pid %d: syscall-452 returned %d\n", getpid(), b);
 
   // syscall453: 実効可能
   b = syscall(453, &a);
-  printf("syscall-452 returned %d\n", b);
+  printf("pid %d: syscall-452 returned %d\n", getpid(), b);
 }
 
 int main() {
@@ -90,12 +90,33 @@ int main() {
     int pid2 = fork();
     if (pid2 == 0) {
       child(sem_id2);
+      
+      sb_semop(sem_id2, UNLOCK);
+
+      // 親プロセスが452を禁止するまで待つ
+      sb_semop(sem_id2, LOCK);
+
+      // syscall452: 実行不可
+      int b;
+      b = syscall(452);
+      printf("pid %d: syscall-452 returned %d\n", getpid(), b);
+  
       return 0;
     }
 
-    printf("ppid=%d, pid=%d\n", pid, pid);
+    printf("pid %d: child pid=%d\n", getpid(), pid2);
     // セマフォをアンロック（子プロセスの処理が始まる）
-    printf("parent process finish.\n");
+    sb_semop(sem_id2, UNLOCK);
+
+    // 子プロセスの処理（sb_semop）を待つ
+    sb_semop(sem_id2, LOCK);
+
+    // 子プロセス2に対し452を禁止
+    int a;
+    a = syscall(460, pid2, 452, 0);
+    printf("pid %d: set syscall-452 to forbidden: %d\n", getpid(), a);
+
+    // 子プロセス2を再開
     sb_semop(sem_id2, UNLOCK);
 
     wait(NULL);
@@ -110,36 +131,35 @@ int main() {
   }
 
   // 親プロセス側の処理
-  printf("ppid=%d, pid=%d\n", ppid, pid);
+  printf("pid %d: child pid=%d\n", getpid(), pid);
   int a;
 
   // 子スレッドをサンドボックスに入れる
   a = syscall(459, pid);
-  printf("register: %d\n", a);
+  printf("pid %d: register: %d\n", getpid(), a);
 
   // Syscall1と2を禁止
   a = syscall(460, pid, 451, 0);
-  printf("set syscall-451 to forbidden: %d\n", a);
+  printf("pid %d: set syscall-451 to forbidden: %d\n", getpid(), a);
   a = syscall(460, pid, 452, 0);
-  printf("set syscall-452 to forbidden: %d\n", a);
+  printf("pid %d: set syscall-452 to forbidden: %d\n", getpid(), a);
 
   // サンドボックスに入っているかチェック＆禁止リストをログに表示
   a = syscall(462, pid);
-  printf("exists: %d\n", a);
+  printf("pid %d: exists: %d\n", getpid(), a);
 
   // Syscall2を許可
   a = syscall(460, pid, 452, 1);
-  printf("set syscall-452 to allow: %d\n", a);
+  printf("pid %d: set syscall-452 to allow: %d\n", getpid(), a);
 
   a = syscall(460, pid, 453, 1);
-  printf("set syscall-453 to allow: %d\n", a);
+  printf("pid %d: set syscall-453 to allow: %d\n", getpid(), a);
 
   // 禁止リストをログに表示
   a = syscall(462, pid);
-  printf("exists: %d\n", a);
+  printf("pid %d: exists: %d\n", getpid(), a);
 
   // セマフォをアンロック（子プロセスの処理が始まる）
-  printf("parent process finish.\n");
   sb_semop(sem_id, UNLOCK);
 
   // 子プロセスの終了を待機
@@ -153,11 +173,11 @@ int main() {
 
   // サンドボックスから取り出す
   a = syscall(461, pid);
-  printf("delete profile: %d\n", a);
+  printf("pid %d: delete profile: %d\n", getpid(), a);
 
   // サンドボックスに入っているかかチェック
   a = syscall(462, pid);
-  printf("exists: %d\n", a);
+  printf("pid %d: exists: %d\n", getpid(), a);
 
   return 0;
 }
